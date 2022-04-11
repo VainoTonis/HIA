583ab150005fd5f9e2a7766339860f0f78eb8f0e
headtrollhunter <headtrollhunter@gmail.com>
2022-04-11 10:14:33 +0300

    first commit

---
+++ initial.py
@@ -0,0 +1,17 @@
+# python
+# Made by sum cunt
+
+from updateSDE import testening
+
+print("Hello")
+  
+# Defining main function
+def main():
+    print("hey there")
+  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
+    testening()
---
+++ updateSDE.py
@@ -0,0 +1,5 @@
+
+
+
+def testening():
+    print("fak")
