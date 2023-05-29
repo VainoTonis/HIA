4a37dcb6b584d54d3b450169a119ddbbc6696569
Tõnis Vaino <vaino.tonis@gmail.com>
2023-05-29 23:35:04 +0300

    remove unnessesary comment

--- EIA/settings.py
+++ EIA/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-#    'externalConnections.externalConnections.ESIAuth',
 ]
 
 ROOT_URLCONF = 'EIA.urls'
