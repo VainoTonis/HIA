e52419416aaa81ec623aac07b4ca9dc5dd75be49
Tõnis Vaino <vaino.tonis@gmail.com>
2023-06-06 00:07:47 +0300

    All resources displayed

--- eia.py
+++ eia.py
@@ -1,65 +1,21 @@
 from PyQt6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView
 from sqlitestuff import getPIData
-from uiElements import planetTextItem,rawResourceTextItem,createConnection
-
-
-def createPlanetTextItems(scene, planetNames):
-    planetTextItems = []
-    for i, planetName in enumerate(planetNames):
-        planetText = planetTextItem(planetName)
-        planetText.setPos(25, 150 + i * 20)
-        scene.addItem(planetText)
-        planetTextItems.append(planetText)
-    return planetTextItems
-
-def createResourceTextItem(scene, resourceName, resourceTextItems):
-    resourceText = next(
-        (item for item in resourceTextItems if item.toPlainText() == resourceName),
-        None,
-    )
-    if resourceText is None:
-        resourceText = rawResourceTextItem(resourceName)
-        resourceText.setPos(200, 150 + len(resourceTextItems) * 20)
-        scene.addItem(resourceText)
-        resourceTextItems.append(resourceText)
-    return resourceText
-
-def createResourceConnection(scene, planetText, resourceText):
-    connection = createConnection(planetText, resourceText)
-    scene.addItem(connection)
-    return connection
-
-def createTextItemsAndConnections(scene, piData, sourceAttribute, targetAttribute):
-    targetTextItems = []
-    processed_planets = set()  # Set to store processed planets
-    # Iterate over the P0 data
-    for p0, p0Data in piData[targetAttribute].items():
-        planetTypes = p0Data[sourceAttribute]
-        
-        # Create planetTextItem for each planet type
-        for planetType in planetTypes:
-            if planetType in processed_planets:
-                continue
-
-            planetText = planetTextItem(planetType)
-            planetText.setPos(25, 150 + len(targetTextItems) * 20)
-            scene.addItem(planetText)
-            targetTextItems.append(planetText)
-            processed_planets.add(planetType)  # Add processed planet to set
-                
-    return targetTextItems
-
+from uiElements import createInitialTextItems,createResourceTextItems
 
 def main():
         
     app = QApplication([])
 
     # Create a QGraphicsScene and set the scene rect
-    scene = QGraphicsScene(0, 0, 800, 600)
+    scene = QGraphicsScene(0, 0, 1100, 800)
     # Extract unique planet names and resource names
     piData = getPIData()
 
-    createTextItemsAndConnections(scene, piData, 'planetTypes', 'P0')
+    createInitialTextItems(scene, piData)
+    createResourceTextItems(scene, piData, "P1", 275)
+    createResourceTextItems(scene, piData, "P2", 450)
+    createResourceTextItems(scene, piData, "P3", 650)
+    createResourceTextItems(scene, piData, "P4", 850)
 
     # Create a QGraphicsView and set the scene
     view = QGraphicsView(scene)
--- uiElements.py
+++ uiElements.py
@@ -44,57 +44,94 @@ class hoverableTextItem(QGraphicsTextItem):
                         connection.src_item.setDefaultTextColor(QColor('gray'))
         super().hoverLeaveEvent(event)
 
-
-
-
 class planetTextItem(hoverableTextItem):
     def __init__(self, text):
         super().__init__(text)
         self.lines = {}  # Store lines in a dictionary
-
 
 class rawResourceTextItem(hoverableTextItem):
     def __init__(self, text):
         super().__init__(text)
         self.lines = {}  # Store lines in a dictionary
 
+class resourceTextItem(hoverableTextItem):
+    def __init__(self, text):
+        super().__init__(text)
+        self.lines = {}  # Store lines in a dictionary
+    # White = Planet
+    # Yellow = Resource
+    # Green = Tier 1 Product (P1)
+    # Aqua = Tier 2 Product (P2)
+    # Blue = Tier 3 Product (P3)
+    # Pink = Tier 4 Product (P4)
 
-def createConnection(planetText, resourceText):
+
+def createConnection(scene, ingredient, ingredientColour, product, productColour):
     connection = QGraphicsLineItem(
-        planetText.pos().x() + planetText.boundingRect().width(),
-        planetText.pos().y() + planetText.boundingRect().height() / 2,
-        resourceText.pos().x(),
-        resourceText.pos().y() + resourceText.boundingRect().height() / 2,
+        ingredient.pos().x() + ingredient.boundingRect().width(),
+        ingredient.pos().y() + ingredient.boundingRect().height() / 2,
+        product.pos().x(),
+        product.pos().y() + product.boundingRect().height() / 2,
     )
 
     gradient = QLinearGradient(
-        planetText.pos().x() + planetText.boundingRect().width(),
-        planetText.pos().y() + planetText.boundingRect().height() / 2,
-        resourceText.pos().x(),
-        resourceText.pos().y() + resourceText.boundingRect().height() / 2,
+        ingredient.pos().x() + ingredient.boundingRect().width(),
+        ingredient.pos().y() + ingredient.boundingRect().height() / 2,
+        product.pos().x(),
+        product.pos().y() + product.boundingRect().height() / 2,
     )
     
-    gradient.setColorAt(0.0, QColor('white'))  # Color at the planet end
-    gradient.setColorAt(1.0, QColor('yellow'))  # Color at the resource end
-
-# White = Planet
-# Yellow = Resource
-# Green = Tier 1 Product (P1)
-# Aqua = Tier 2 Product (P2)
-# Blue = Tier 3 Product (P3)
-# Pink = Tier 4 Product (P4)
+    gradient.setColorAt(0.0, QColor(ingredientColour))   # Colour at the ingredient end
+    gradient.setColorAt(1.0, QColor(productColour))   # Colour at the product end
 
     pen = QPen(QBrush(gradient), 1)  # Create a pen with the gradient brush
     connection.setPen(pen)
 
-    connection.setVisible(False)  # Set the initial visibility to Fals
-    connection.src_item = planetText  # Store the source item in the connection object
-    connection.dest_item = resourceText  # Store the destination item in the connection object
+    connection.setVisible(False)  # Set the initial visibility to False
+    connection.src_item = ingredient  # Store the source item in the connection object
+    connection.dest_item = product  # Store the destination item in the connection object
 
-    planetText.connections.append(connection)
-    resourceText.connections.append(connection)
+    ingredient.connections.append(connection)
+    product.connections.append(connection)
 
+    scene.addItem(connection)
     return connection
 
 
+def createResourceTextItems(scene, piData, endProductLevel, staticGap):
+    targetTextItems = []
+    processedResources = set()  # Set to store processed planets
+    # Iterate over the P0 data
+    for i in piData[endProductLevel].items():
+        resourceText = rawResourceTextItem(i[0])
+        resourceText.setPos(staticGap , 150 + len(targetTextItems) * 20)
+        scene.addItem(resourceText)
+        targetTextItems.append(resourceText)
+        processedResources.add(i[0])  # Add processed planet to set
+    
+    return targetTextItems
 
+# This is to display both Planet and P0 resources
+def createInitialTextItems(scene, piData):
+    targetTextItems = []
+    rawResourceTextItems = []
+    processed_planets = set()  # Set to store processed planets
+    # Iterate over the P0 data
+    for rawResource, planetType in piData["P0"].items():
+        planetTypes = planetType["planetTypes"]
+        
+        # Create planetTextItem for each planet type
+        for planetType in planetTypes:
+            if planetType in processed_planets:
+                continue
+
+            planetText = planetTextItem(planetType)
+            planetText.setPos(25 , 150 + len(targetTextItems) * 20)
+            scene.addItem(planetText)
+            targetTextItems.append(planetText)
+            processed_planets.add(planetType)  # Add processed planet to set
+    
+        rawResourceText = rawResourceTextItem(rawResource)
+        rawResourceText.setPos(125, 150 + len(rawResourceTextItems) * 20)
+        scene.addItem(rawResourceText)
+        rawResourceTextItems.append(rawResourceText)
